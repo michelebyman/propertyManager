2ddbf9d7f0cfa4bc503ad6e62b19d507439ec413
Michele Byman <michelebyman@gmail.com>
2020-10-13 10:22:40 +0200

    Initial Commit

---
+++ propertyManger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  propertyManger
+//
+//  Created by Michele Byman on 2020-10-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
